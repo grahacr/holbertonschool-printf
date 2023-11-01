4e56b831bf5442bdb6f919f56c05827371f2fb32
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-01 12:59:44 -0700

    First day struggles

--- main.h
+++ main.h
@@ -1,6 +1,15 @@
 #ifndef main_h
 #define main_h
+
 int _putchar(char c);
-
-
+typedef struct spec
+{
+  char *spec;
+			  char (*f)(char a, ...);
+} spec_t
+char Spec_c(const char *format, unsigned int n)
+ char Spec_s(const char *format, ...);
+int Spec_%(const char *format, ...);
+Spec_d(const char *format, ...); 
+Spec_i(const char *format, ...);
 #endif
