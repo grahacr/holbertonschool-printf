69ac742d894ece2ba2116b0b98a8653279b8f06f
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-02 11:01:28 -0700

    Main.h prototype add

--- main.h
+++ main.h
@@ -1,15 +1,19 @@
 #ifndef main_h
 #define main_h
-
-int _putchar(char c);
+/**
+ *
+ *
+ */
 typedef struct spec
 {
-  char *spec;
-			  char (*f)(char a, ...);
-} spec_t
-char Spec_c(const char *format, unsigned int n)
- char Spec_s(const char *format, ...);
-int Spec_%(const char *format, ...);
-Spec_d(const char *format, ...); 
-Spec_i(const char *format, ...);
+char *spec;
+char (*f)(char a, ...);
+} spec_t;
+int spec_ch(const char *format, ...)
+int spec_s(const char *format, ...);
+int spec_spec(const char *format, ...);
+int spec_d(const char *format, ...); 
+int spec_i(const char *format, ...);
+int (*get_spec_func(char *s))(const char *format, ...)
+int _printf(const char *format, ...);
 #endif
