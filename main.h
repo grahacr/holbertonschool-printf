c1e648754b9b11e407fd5085a40c0fad29f255b3
Courtney <132097902+grahacr@users.noreply.github.com>
2023-11-03 12:36:38 -0500

    Update main.h
    clean up prototypes and structure definition

--- main.h
+++ main.h
@@ -9,15 +9,13 @@
 typedef struct spec
 {
 char *spec;
-int (*f)(int, const char *);
-int (*f)(void*, void);
+int (*f)(va_list);
 } spec_t;
-int print_format(char specifier, va_list ap);
 int print_char(int c);
 int print_string(char *str);
 int print_spec(char c);
 int print_digit(long n, int base);
 int print_integer(int c);
-int (*get_spec_func(char *s))(void *, void *);
+int (*get_spec_func(char *s))(va_list);
 int _printf(const char *format, ...);
 #endif
