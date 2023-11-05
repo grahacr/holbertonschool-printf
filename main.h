6c365a520f0a969fe1c9c8c54a97a034b9373e70
Courtney <132097902+grahacr@users.noreply.github.com>
2023-11-05 14:08:55 -0600

    Update main.h
    get rid of unnecessary special character function

--- main.h
+++ main.h
@@ -12,7 +12,6 @@ int (*f)(va_list);
 } spec_t;
 int print_char(va_list ap);
 int print_string(va_list ap);
-int print_spec(va_list ap);
 int (*get_spec_func(const char *s))(va_list ap);
 int _printf(const char *format, ...);
 #endif
