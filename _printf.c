405562b7301ec58427cc0652bfc711ac13c43d08
Courtney <132097902+grahacr@users.noreply.github.com>
2023-11-05 14:53:44 -0600

    Update _printf.c
    change written to purchar statements

--- _printf.c
+++ _printf.c
@@ -18,7 +18,7 @@ int _printf(const char *format, ...)
 			format++;
 			if (*format == '%')
 			{
-				i += write(1, "%", 1);
+				putchar(*format);
 			}
 			else
 			{
@@ -32,7 +32,7 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			i += write(1, *format, 1);
+			putchar(*format);
 		}
 		format++;
 	}
