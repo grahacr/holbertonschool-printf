3399fc789bed12a8a54b5ca67577b295c75627ef
Courtney <132097902+grahacr@users.noreply.github.com>
2023-11-05 13:50:16 -0600

    Update _printf.c
    fix if statement again

--- _printf.c
+++ _printf.c
@@ -20,7 +20,7 @@ int _printf(const char *format, ...)
 			{
 				int (*written)(va_list);
 				written = get_spec_func(format);
-				while (*written)
+				if (*written)
 				{
 					i += written(ap);
 				}
