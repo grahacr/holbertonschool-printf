ae7f99cdf3a7f3f845aab67ab3e1eada3358db04
VengeanceIsHis <elijahjpoole@gmail.com>
2023-11-06 18:42:32 -0800

    Betty fix

--- functions.c
+++ functions.c
@@ -5,45 +5,60 @@
 #include <unistd.h>
 #include <string.h>
 /**
- *
- *
+ *print_char - function that prints the given argument
+ *@ap: argument passed to the function
+ *Return: Always 1
  */
 int print_char(va_list ap)
 {
-	char c = va_arg(ap, int);
-	putchar(c);
-	return (1);
+  char c = va_arg(ap, int);
+  putchar(c);
+  return (1);
 }
+/**
+ *print_string - function that prints the given string
+ *@ap: argument passed to the function
+ *Return: 6 for NULL or length of string passed
+ */
 int print_string(va_list ap)
 {
-	char *str = va_arg(ap, char*);
-	int len = 0;
-	int i;
-
-	if (str != NULL)
+  char *str = va_arg(ap, char*);
+  int len = 0;
+  int i;
+  if (str != NULL)
+    {
+      len = strlen(str);
+      for (i = 0; i < len; i++)
 	{
-		len = strlen(str);
-		for (i = 0; i < len; i++)
-		{
-			putchar(str[i]);
-		}
+	  putchar(str[i]);
 	}
-	else
-	{
-		str = "(null)";
-		len = 6;
-		for (i = 0; i < len; i++)
-		{
-			putchar(str[i]);
-		}
-	}
-	return (len);
+    }
+  else
+    {
+      str = "(null)");
+  len = 6;
+  for (i = 0; i < len; i++)
+    {
+      putchar(str[i]);
+    }
 }
+return (len);
+}
+/**
+ *print_2 - function that returns 2 for wrong specifier given
+ *@ap: unused input 
+ *Return: 2 for characters printed
+ */
 int print_2(va_list ap)
 {
   (void) ap;
   return (2);
 }
+/**
+ *print_null - function that returns -1 for Null specifier
+ *@ap: unused variable for function
+ *Return: -1 for Error specifier
+ */
 int print_null(va_list ap)
 {
   (void) ap;
--- spec_functions.c
+++ spec_functions.c
@@ -4,27 +4,27 @@
 #include <string.h>
 #include <stdarg.h>
 /**
- *
- *
- *
+ *get_spec_func - function pointer that gathers which function to print
+ *@s: format specifier input that seeks the corresponding function
+ *Return: function that corresponds with the specifier
  */
 int (*get_spec_func(const char *s))(va_list ap)
 {
   spec_t specs[] = {
-	  { 'c', print_char },
-	  { 's', print_string },
-	  { '\0', print_null },
-	  { 'i', print_digit },
-	  { 'd', print_digit }
+		    { 'c', print_char },
+		    { 's', print_string },
+		    { '\0', print_null },
+		    { 'i', print_digit },
+		    { 'd', print_digit }
   };
   int i = 0;
   while (i < 5)
     {
       if (specs[i].spec == *s)
-      {
-        return (specs[i].f);
-      }
-	i++;
+	{
+	  return (specs[i].f);
+	}
+      i++;
     }
   putchar('%');
   putchar(*s);
